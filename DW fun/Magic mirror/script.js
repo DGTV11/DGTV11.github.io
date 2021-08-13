646bd2365da387cad84e1c89157aea53742f39b0
DGTV11 <danielweedk@gmail.com>
2021-08-13 11:24:37 +0800

    .

---
+++ DW fun/Magic mirror/script.js
@@ -0,0 +1,17 @@
+if (confirm('Start Magic Mirror?') === true) {
+    var video = document.querySelector('video')
+    var select = document.querySelector('select')
+
+    function changeEffect() {
+        video.style.filter = select.value
+    }
+    select.addEventListener('change', changeEffect)
+
+    navigator.mediaDevices
+        .getUserMedia({
+            video: true,
+        })
+        .then(function (stream) {
+            video.srcObject = stream
+        })
+}
